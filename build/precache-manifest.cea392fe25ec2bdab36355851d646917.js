96d0b4b1c7164749b631433e01ef196f48379298
Auxilia-vinnarasi <83941036+Auxilia-vinnarasi@users.noreply.github.com>
2021-12-28 20:38:21 +0530

    Add files via upload

---
+++ build/precache-manifest.cea392fe25ec2bdab36355851d646917.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3913b2e4c40dee03dd7abfb45165bd6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0dbc142f991248c7fcd",
+    "url": "/static/css/main.98346d44.chunk.css"
+  },
+  {
+    "revision": "bfdc900b4e5d794d2da6",
+    "url": "/static/js/2.bb0d443c.chunk.js"
+  },
+  {
+    "revision": "a0dbc142f991248c7fcd",
+    "url": "/static/js/main.9d7c7b6d.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
